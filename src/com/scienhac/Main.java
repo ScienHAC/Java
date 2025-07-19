53cc6b5d2440b6d2b2b25617b7dcd9cd21685d13
Piyush Sharma <piyushkumar40515s@gmail.com>
2025-07-19 18:41:38 +0530

    Java Journey

---
+++ src/com/scienhac/Main.java
@@ -0,0 +1,12 @@
+package com.scienhac;// packages used to group related classes and it generally for organization
+
+public class Main { // classes are in PascalCase
+    public static void main(String[] args) { // for function names, we use camelCase
+        System.out.println("Hello, World! from the system nice to meet you!");
+        int $fun; // $ is allowed in variable names, but not recommended
+        int _fun; // _ is allowed in variable names, but not recommended
+    }
+}
+//addTwoNumber is camelCase
+//AddTwoNumber is PascalCase
+//char uses unicode characters with UTF - 16 encoding and it is 2 bytes long where normal char is 1 byte long and uses ASCII encoding or we say UTF - 8 encoding
