6fb838766f04518cd90813cfd1369883ef5df5dd
Piyush Sharma <piyushkumar40515s@gmail.com>
2025-08-03 10:02:15 +0530

    Update code

--- src/com/scienhac/third/Get_Started.java
+++ src/com/scienhac/third/Get_Started.java
@@ -6,14 +6,19 @@ public class Get_Started {
     public static void main(String[] args) {
         System.out.println("Welcome" + " world"); // String concatenation using + operator in c++ we can use via cout<<string("the")+"noice"
         Scanner sc = new Scanner(System.in), test = new Scanner(System.in); // Creating a Scanner object to read input from the console
-        if (sc.hasNextInt()) {
-            int value = sc.nextInt();   // Safely read the int
-            System.out.println("You entered: " + value);
-        } else {
-            System.out.println("That's not an integer!");
-        }
+//        if (sc.hasNextInt()) {
+//            int value = sc.nextInt();   // Safely read the int
+//            System.out.println("You entered: " + value);
+//        } else {
+//            System.out.println("That's not an integer!");
+//        }
+//
+//        int x = test.nextInt();
+//        System.out.println("You entered: " + x);
+        double y = 3.75;
+        System.out.println(4 % 2.1);
+        System.out.println(y % 2.1);
+        System.out.println(y % 2);
 
-        int x = test.nextInt();
-        System.out.println("You entered: " + x);
     }
 }
