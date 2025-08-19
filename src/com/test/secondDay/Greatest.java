d7eac163f2bffa2322447073ecdab21fc405b43b
Shweta Sharma <shwetasharma9914@gmail.com>
2025-08-19 10:06:09 +0530

    Basic test code added

---
+++ src/com/test/secondDay/Greatest.java
@@ -0,0 +1,17 @@
+package com.test.secondDay;
+
+public class Greatest {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30;
+        int greatest = a; // Assume a is the greatest initially
+
+        if (b > greatest) {
+            greatest = b; // Update if b is greater
+        }
+        if (c > greatest) {
+            greatest = c; // Update if c is greater
+        }
+
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
---
+++ src/com/test/secondDay/leapYear.java
@@ -0,0 +1,15 @@
+package com.test.secondDay;
+import java.util.Scanner;
+public class leapYear {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year to check if it is a leap year: ");
+        int Year = sc.nextInt();
+        if((Year%4==0 && Year%100!=0) || Year%400==0){
+            System.out.println(Year + " is a leap year.");
+        } else {
+            System.out.println(Year + " is not a leap year.");
+        }
+        sc.close();
+    }
+}
