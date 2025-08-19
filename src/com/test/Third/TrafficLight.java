f239972b80cbe4f2dca79bd17031b534a34b46c9
Shweta Sharma <shwetasharma9914@gmail.com>
2025-08-19 10:20:58 +0530

    Basic test code added

---
+++ src/com/test/Third/TrafficLight.java
@@ -0,0 +1,23 @@
+package com.test.Third;
+import java.util.Scanner;
+public class TrafficLight {
+    public static void main(String[] args){
+        System.out.println("Traffic Light Simulation enter choice to change color: ");
+        Scanner sc = new Scanner(System.in);
+        int lightColor = sc.nextInt();
+
+        switch (lightColor) {
+            case 1:
+                System.out.println("Go");
+                break;
+            case 2:
+                System.out.println("Slow down");
+                break;
+            case 3:
+                System.out.println("Stop");
+                break;
+            default:
+                System.out.println("Invalid color");
+        }
+    }
+}
