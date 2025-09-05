8e93e6cd1d34a9cf767cac4fb9b6d97c4a8fce89
Shweta Sharma <shwetasharma9914@gmail.com>
2025-09-05 14:13:19 +0530

    Basic test code added

---
+++ src/com/scienhac/lectures/lec1/Main.java
@@ -0,0 +1,5 @@
+package com.scienhac.lectures.lec1;
+
+public class Main {
+//    already done .......
+}
---
+++ src/com/scienhac/lectures/lec2/Var.java
@@ -0,0 +1,30 @@
+package com.scienhac.lectures.lec2;
+import java.util.*;
+public class Var {
+//    No of data types in Java
+/*
+Primitive              Derived
+byte                    string
+char                    Array
+boolean                 Class
+String                  Object
+float                   Interface
+Double
+long
+int
+short
+ */
+    Scanner sc = new Scanner(System.in);
+    /*
+    Ways of taking input from user
+    -->sc.nextInt(); // for int
+    -->sc.nextFloat(); // for float
+    -->sc.nextDouble(); // for double
+    -->sc.nextLine(); // for string
+    -->sc.next().charAt(0); // for char
+    -->sc.nextBoolean(); // for boolean
+    -->sc.nextLong(); // for long
+    -->sc.nextByte(); // for byte
+    -->sc.nextShort(); // for short
+     */
+}
