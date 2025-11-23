7bf9e7f70ced20f294bd2320d433f487b551411d
Piyush Sharma <piyushkumar40515s@gmail.com>
2025-11-23 12:51:08 +0530

    Update code

--- src/com/scienhac/codewithharry/lec71Runnable/Main.java
+++ src/com/scienhac/codewithharry/lec71Runnable/Main.java
@@ -25,6 +25,22 @@ public class Main {
         MyThread2 bullet2 = new MyThread2();
         Thread gun2 = new Thread(bullet2);
         Thread gun3 = new Thread(bullet2);
+        Runnable bullet4 = () -> { // it work only with functional interface and these is lambda expression
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("MyThread3 is running " + i);
+            }
+        };
+        Runnable bullet5 = new Runnable() { //it can use any interface but all methods should be implemented
+            @Override
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println("MyThread4 is running " + i);
+                }
+            }
+        };
+        Thread gun4 = new Thread(bullet4, "Gun-4");
+        Thread gun5 = new Thread(bullet5, "Gun-5");
+        gun4.start();
 //        bullet`.start(); not works because start method is not in Runnable interface
         gun1.start();
         gun2.start();
